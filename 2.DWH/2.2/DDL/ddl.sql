7a24e9bab5d3bdd9dd230aaa3234de7905d9f0e6
”Martynov <Martynov-platon@mail.ru>
2023-06-22 22:19:03 +0300

    changes in phys structure

--- 2.DWH/2.2/DDL/ddl.sql
+++ 2.DWH/2.2/DDL/ddl.sql
@@ -7,28 +7,19 @@ CREATE TABLE IF NOT EXISTS public.readers (
 
 CREATE TABLE IF NOT EXISTS public.books (
     id_book serial PRIMARY KEY,
-    book_name varchar(255),
-    author varchar(255),
+    book_name varchar(255) NOT NULL,
     create_date date,
     sheets smallint,
     cost integer,
-    amount integer
+    amount integer NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS public.rent (
+CREATE TABLE IF NOT EXISTS public.transaction (
     id_rent serial PRIMARY KEY,
-    id_book integer,
-    id_reader integer,
-    date_rent date,
-    FOREIGN KEY (id_book) REFERENCES books (id_book) ON DELETE SET NULL,
-    FOREIGN KEY (id_reader) REFERENCES readers (id_reader) ON DELETE SET NULL
-);
-
-CREATE TABLE IF NOT EXISTS public.return (
-    id_rent serial PRIMARY KEY,
-    id_book integer,
-    id_reader integer,
-    date_return date,
+    id_book integer NOT NULL,
+    id_reader integer NOT NULL,
+    rent_date date,
+    return_date date,
     FOREIGN KEY (id_book) REFERENCES books (id_book) ON DELETE SET NULL,
     FOREIGN KEY (id_reader) REFERENCES readers (id_reader) ON DELETE SET NULL
 );
@@ -40,8 +31,21 @@ CREATE TABLE IF NOT EXISTS public.publishing_office (
 
 CREATE TABLE IF NOT EXISTS public.publishing_book (
     id_publishing serial PRIMARY KEY,
-    id_book integer,
-    office_name varchar(255),
+    id_book integer NOT NULL,
+    office_name varchar(255) NOT NULL,
     FOREIGN KEY (id_book) REFERENCES books (id_book) ON DELETE SET NULL,
     FOREIGN KEY (office_name) REFERENCES publishing_office (office_name) ON DELETE SET NULL
+);
+
+CREATE TABLE IF NOT EXISTS public.author (
+    id_author serial PRIMARY KEY,
+    author varchar(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS public.book_author (
+    id_book_author serial PRIMARY KEY,
+    id_book integer NOT NULL,
+    id_author integer NOT NULL,
+    FOREIGN KEY (id_book) REFERENCES books (id_book) ON DELETE SET NULL,
+    FOREIGN KEY (id_author) REFERENCES author (id_author) ON DELETE SET NULL
 );
